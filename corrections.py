8502f0522abd0bb39cf02bd21831488f8d4de387
Riku55 <53604972+Riku55@users.noreply.github.com>
2019-10-03 11:41:43 +0200

    Correct wrong game titles (to be expanded)

---
+++ corrections.py
@@ -0,0 +1,4 @@
+correction_list = {}
+
+correction_list["Legend of Zelda, The - Majora's Mask"] = "The Legend of Zelda - Majora's Mask"
+correction_list["Legend of Zelda, The - Ocarina of Time"] = "The Legend of Zelda - Ocarina of Time"
