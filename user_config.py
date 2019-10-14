2b5f5201aab239836cc247c155b87fb7a0c3a21c
Riku55 <53604972+Riku55@users.noreply.github.com>
2019-10-14 17:25:53 +0200

    clarified use

--- user_config.py
+++ user_config.py
@@ -1,4 +1,4 @@
-#enter path for your roms and RetroArch here. Be sure to add a "/" at the end of each path.
+#enter path for your roms and RetroArch here. Put them between the quotation marks the Be sure to add a "/" at the end of each path.
 #example:
 #emu_path = "C:/Users/USERNAME/AppData/Roaming/RetroArch/"
 
