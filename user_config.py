2e337f9e771c365df8114ab03068380649e21e20
Riku55 <53604972+Riku55@users.noreply.github.com>
2019-10-17 11:37:22 +0200

    additional instructions

--- user_config.py
+++ user_config.py
@@ -1,9 +1,11 @@
-#enter path for your roms and RetroArch here. Put them between the quotation marks the Be sure to add a "/" at the end of each path.
-#example:
-#emu_path = "C:/Users/USERNAME/AppData/Roaming/RetroArch/"
+# enter path for your roms and RetroArch here. Put them between the quotation marks.
+# Be sure to add a "/" at the end of each path.
+# example: (this is also the standard RetroArch path, so if you didn't choose a specific folder for installing it,
+# you can just copy the example path)
+# emu_path = "C:/Users/USERNAME/AppData/Roaming/RetroArch/"
 
-rom_path = ""
 emu_path = ""
+rom_path = ""
 
 #Select the core you want to use. Standard is Mupen64Plus_Next.
 # Mupen64Plus_Next = 1
