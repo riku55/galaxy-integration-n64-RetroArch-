16a9e605c8c92792e6f87054f475479b041bc461
Riku55 <53604972+Riku55@users.noreply.github.com>
2019-10-25 18:14:18 +0200

    added list for region specific names

--- corrections.py
+++ corrections.py
@@ -6,3 +6,7 @@ correction_list["Doubutsu no Mori"] = "Animal Forest"
 correction_list["Bomberman 64 - The Second Attack!"] = "Bomberman 64: The Second Attack"
 correction_list["Tarzan"] = "Disney's Tarzan"
 correction_list["RR64 - Ridge Racer 64"] = "Ridge Racer 64"
+
+correction_list2 = {}
+
+correction_list2["Pokemon Stadium (Japan)"] = "Pocket Monsters Stadium"
