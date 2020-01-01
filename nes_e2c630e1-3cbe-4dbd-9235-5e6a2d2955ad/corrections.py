a6f9dd73d6b050084b8d56953bfcf8469740353b
Jason Shackles <jshackles@gmail.com>
2020-01-01 11:46:25 -0800

    UPDATE: nes corrections
    Will continue adding more as found

--- nes_e2c630e1-3cbe-4dbd-9235-5e6a2d2955ad/corrections.py
+++ nes_e2c630e1-3cbe-4dbd-9235-5e6a2d2955ad/corrections.py
@@ -1,4 +1,6 @@
 correction_list = {}
 
-correction_list["Final Fantasy II"] = "Final Fantasy IV"
-correction_list["Final Fantasy III"] = "Final Fantasy VI"
+correction_list["Dragon Warrior"] = "Dragon Quest"
+correction_list["Dragon Warrior II"] = "Dragon Quest II"
+correction_list["Dragon Warrior III"] = "Dragon Quest III"
+correction_list["Dragon Warrior IV"] = "Dragon Quest IV"
